3a9b73c7741dbdc2c2f8ebba2c3dca779c737719
jtr354 <jtr354@foxmail.com>
2022-07-12 12:56:06 +0800

    fix:time error

--- src/util/createTime.js
+++ src/util/createTime.js
@@ -1,17 +1,30 @@
 export default function createTime() {
   const now = new Date();
   const year = now.getFullYear();
-  const month = pad(now.getDate());
-  const day = pad(now.getDay());
+  const month = pad(now.getMonth() + 1);
+  const day = pad(now.getDate());
   const hour = pad(now.getHours());
   const minutes = pad(now.getMinutes());
   const seconds = pad(now.getSeconds());
   return [year, month, day, hour, minutes, seconds];
-};
+}
 
 export function getNowTime() {
-  const [year,month, day, hour, minutes, seconds] = createTime()
-  return year + '年' + month + '月' + day + '日' + hour + '时' + minutes + '分' + seconds + '秒'
+  const [year, month, day, hour, minutes, seconds] = createTime();
+  return (
+    year +
+    "年" +
+    month +
+    "月" +
+    day +
+    "日" +
+    hour +
+    "时" +
+    minutes +
+    "分" +
+    seconds +
+    "秒"
+  );
 }
 
 function pad(t, maxLength = 2, fullString = "0") {
