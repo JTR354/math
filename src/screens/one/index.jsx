e62bad819030b0235623366b71274455f849acc6
jtr354 <jtr354@foxmail.com>
2023-02-08 18:16:52 +0800

    feat: modify actions

--- src/screens/one/index.jsx
+++ src/screens/one/index.jsx
@@ -66,13 +66,6 @@ export default function VerticalTabs() {
         aria-label="Vertical tabs example"
         sx={{ borderRight: 1, borderColor: "divider" }}
       >
-        {/* <Tab label="Item One" {...a11yProps(0)} />
-        <Tab label="Item Two" {...a11yProps(1)} />
-        <Tab label="Item Three" {...a11yProps(2)} />
-        <Tab label="Item Four" {...a11yProps(3)} />
-        <Tab label="Item Five" {...a11yProps(4)} />
-        <Tab label="Item Six" {...a11yProps(5)} />
-        <Tab label="Item Seven" {...a11yProps(6)} /> */}
         {list.map((_, i) => {
           return <Tab key={i} label={`${i + 1}的乘法`} {...a11yProps(i)} />;
         })}
@@ -80,31 +73,10 @@ export default function VerticalTabs() {
       {list.map((_, i) => {
         return (
           <TabPanel key={i} value={value} index={i}>
-            <PanelItem value={value} />
+            <PanelItem value={value} setValue={setValue} />
           </TabPanel>
         );
       })}
-      {/* <TabPanel value={value} index={0}>
-        Item One
-      </TabPanel>
-      <TabPanel value={value} index={1}>
-        Item Two
-      </TabPanel>
-      <TabPanel value={value} index={2}>
-        Item Three
-      </TabPanel>
-      <TabPanel value={value} index={3}>
-        Item Four
-      </TabPanel>
-      <TabPanel value={value} index={4}>
-        Item Five
-      </TabPanel>
-      <TabPanel value={value} index={5}>
-        Item Six
-      </TabPanel>
-      <TabPanel value={value} index={6}>
-        Item Seven
-      </TabPanel> */}
     </Box>
   );
 }
--- src/screens/one/panel.jsx
+++ src/screens/one/panel.jsx
@@ -3,18 +3,22 @@ import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import styled from "@emotion/styled";
 import Button from "@mui/material/Button";
+import ButtonGroup from "@mui/material/ButtonGroup";
 
 const Item = styled.div`
   display: flex;
+  align-items: center;
   & input {
     text-align: center;
+    font-weight: bold;
+    font-size: 2rem;
   }
   & p[class*="Mui-error"] {
     white-space: nowrap;
   }
 `;
 
-export default function ValidationTextFields({ value }) {
+export default function ValidationTextFields({ value, setValue }) {
   const number = value + 1;
   const [list] = React.useState(new Array(9).fill(0));
   const [reset, toReset] = React.useReducer((x) => x + 1, 0);
@@ -35,14 +39,24 @@ export default function ValidationTextFields({ value }) {
           </Item>
         );
       })}
-      <Button
-        variant="contained"
-        onClick={() => {
-          toReset();
-        }}
-      >
-        再来一次
-      </Button>
+      <ButtonGroup>
+        <Button
+          variant="contained"
+          onClick={() => {
+            toReset();
+          }}
+        >
+          再来一次
+        </Button>
+        <Button
+          onClick={() => {
+            const index = (value + 1) % list.length;
+            setValue(index);
+          }}
+        >
+          下一个难度
+        </Button>
+      </ButtonGroup>
     </Box>
   );
 }
@@ -50,8 +64,11 @@ export default function ValidationTextFields({ value }) {
 function FieldItem({ number, count, reset }) {
   const [value, setValue] = React.useState("");
   const isError = React.useMemo(() => {
-    if (!value) return false;
+    if (value === "") return false;
     return number * count !== +value;
+  }, [number, count, value]);
+  const isSuccess = React.useMemo(() => {
+    return number * count === +value;
   }, [number, count, value]);
   React.useEffect(() => {
     setValue("");
@@ -62,27 +79,42 @@ function FieldItem({ number, count, reset }) {
       <TextField
         style={{ width: "4em", textAlign: "center" }}
         defaultValue={number}
+        // color="secondary"
+        // focused
         disabled
+        InputProps={{
+          readOnly: true,
+        }}
       />
+      ×
       <TextField
         style={{ width: "4em", textAlign: "center" }}
         defaultValue={count}
+        // color="warning"
+        // focused
         disabled
+        InputProps={{
+          readOnly: true,
+        }}
       />
+      ＝
       <TextField
         style={{ width: "4em", textAlign: "center" }}
         error={isError}
         autoComplete="false"
         value={value}
         autoFocus={count === number}
-        type="number"
+        type="tel"
+        color={isSuccess ? "success" : ""}
+        label={isSuccess ? "√" : isError ? "×" : ""}
+        focused
         onChange={(e) => {
           setValue(e.target.value);
         }}
         onFocus={(e) => {
           e.target.select();
         }}
-        helperText={isError ? "就差一点了！" : ""}
+        // helperText={isError ? "就差一点了！" : ""}
       />
     </>
   );
